a76d06691d6d9a45145e2b00a2ddedc7c7d6c3e5
ultimate <106779544+0xultimate@users.noreply.github.com>
2023-05-28 13:07:46 +1000

    feat: mvp for snaps

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,9 @@
-import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import {
+  OnRpcRequestHandler,
+  OnTransactionHandler,
+} from '@metamask/snaps-types';
+import { NodeType, divider, heading, panel, text } from '@metamask/snaps-ui';
+import { Json } from '@metamask/utils';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -31,3 +35,192 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
       throw new Error('Method not found.');
   }
 };
+
+type SimulateRequest = {
+  id: string;
+  chainID: string;
+  signer: string;
+  origin: string;
+  method: string;
+  transaction: {
+    [key: string]: Json;
+  };
+};
+
+type StateChange = {
+  assetType: string;
+  changeType: string;
+  address: string;
+  amount: string;
+  symbol: string;
+  decimals: number;
+  contractAddress: string;
+  name: string;
+  logo: string;
+  tokenID: string;
+  tokenURI: string;
+  tokenName: string;
+  openSeaFloorPrice: number;
+  openSeaVerified: boolean;
+  openSeaLink: string;
+  etherscanVerified: boolean;
+  etherscanLink: string;
+  coinmarketcapLink: string;
+  message: string;
+  fiatValue: string;
+};
+
+// Handle outgoing transactions.
+export const onTransaction: OnTransactionHandler = async ({
+  transaction,
+  chainId,
+  transactionOrigin,
+}) => {
+  console.log('transaction', transaction);
+
+  switch (chainId) {
+    case 'eip155:1':
+      chainId = '1';
+      break;
+  }
+
+  // Make a request to the simulator
+  const simulateRequest: SimulateRequest = {
+    id: '1',
+    chainID: chainId,
+    signer: transaction.from as string,
+    origin: transactionOrigin as string,
+    method: 'eth_sendTransaction',
+    transaction: transaction,
+  };
+
+  console.log('simulateRequest', simulateRequest);
+
+  const response = await fetch(
+    'http://localhost:8081/v0/eth/mainnet/transaction',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(simulateRequest),
+    },
+  );
+  const json = await response.json();
+  console.log('response', JSON.stringify(json));
+
+  console.log(
+    'componentController',
+    JSON.stringify(ComponentController(json.stateChanges)),
+  );
+  // Display percentage of gas fees in the transaction insights UI.
+  return {
+    content: panel(ComponentController(json.stateChanges)),
+  };
+};
+
+const ComponentController = (stateChanges: StateChange[]) => {
+  console.log('stateChanges', JSON.stringify(stateChanges));
+  var receiveComponents: (
+    | {
+        value: string;
+        type: NodeType.Copyable;
+      }
+    | {
+        type: NodeType.Divider;
+      }
+    | {
+        value: string;
+        type: NodeType.Heading;
+      }
+    | {
+        type: NodeType.Spinner;
+      }
+    | {
+        value: string;
+        type: NodeType.Text;
+      }
+  )[] = [heading('You will receive:')];
+
+  var transferComponents: (
+    | {
+        value: string;
+        type: NodeType.Copyable;
+      }
+    | {
+        type: NodeType.Divider;
+      }
+    | {
+        value: string;
+        type: NodeType.Heading;
+      }
+    | {
+        type: NodeType.Spinner;
+      }
+    | {
+        value: string;
+        type: NodeType.Text;
+      }
+  )[] = [heading('You will send:')];
+
+  // Sort state changes into receive and withdrawn
+  stateChanges.forEach((stateChange: StateChange) => {
+    switch (stateChange.changeType) {
+      case 'RECEIVE':
+        receiveComponents.push(...ComponentFactory(stateChange));
+        break;
+      case 'TRANSFER':
+        transferComponents.push(...ComponentFactory(stateChange));
+        break;
+    }
+  });
+  console.log('receiveComponents', JSON.stringify(receiveComponents));
+  console.log('transferComponents', JSON.stringify(transferComponents));
+  return [...receiveComponents, divider(), ...transferComponents];
+};
+
+const ComponentFactory = (stateChange: StateChange) => {
+  var components: (
+    | {
+        value: string;
+        type: NodeType.Copyable;
+      }
+    | {
+        type: NodeType.Divider;
+      }
+    | {
+        value: string;
+        type: NodeType.Heading;
+      }
+    | {
+        type: NodeType.Spinner;
+      }
+    | {
+        value: string;
+        type: NodeType.Text;
+      }
+  )[] = [];
+  switch (stateChange.assetType) {
+    case 'NATIVE':
+    case 'ERC1155':
+    case 'ERC20':
+      components.push(
+        text(
+          `${stateChange.amount} ${stateChange.symbol} ($${stateChange.fiatValue})`,
+        ),
+      );
+      break;
+    case 'ERC721':
+      components.push(
+        text(`${stateChange.tokenName} ($${stateChange.fiatValue})`),
+      );
+      break;
+    case 'Security':
+      SecurityComponent(stateChange);
+      break;
+  }
+  console.log('components', JSON.stringify(components));
+  return components;
+};
+
+const SecurityComponent = (stateChange: StateChange) => {};
