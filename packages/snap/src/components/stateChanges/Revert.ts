67fe9b16dac531350c86d52c81192bb390583260
mindofmar <martinpeko777@gmail.com>
2023-06-15 00:16:06 -0500

    boost preventative measures against ddos

--- packages/snap/src/components/stateChanges/Revert.ts
+++ packages/snap/src/components/stateChanges/Revert.ts
@@ -4,7 +4,7 @@ import { heading, panel, text } from '@metamask/snaps-ui';
 export const RevertComponent = (): OnTransactionResponse => {
   return {
     content: panel([
-      heading('Revert'),
+      heading('Revert warning'),
       text(
         'The transaction will be reverted and your gas fee will go to waste.',
       ),
