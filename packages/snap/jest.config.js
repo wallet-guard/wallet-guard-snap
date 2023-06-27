28e8dc7ebcb9a6fe539b5492bc2fda08aa283d34
mindofmar <martinpeko777@gmail.com>
2023-06-27 12:02:17 -0500

    try adding snaps-jest package

---
+++ packages/snap/jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  preset: '@metamask/snaps-jest',
+};
---
+++ packages/snap/src/index.test.ts
@@ -0,0 +1,10 @@
+import { expect } from '@jest/globals';
+import { installSnap } from '@metamask/snaps-jest';
+
+import { panel, text } from '@metamask/snaps-ui';
+
+describe('onTransaction', () => {
+  it('returns transaction insights for an ERC-20 transaction', async () => {
+    const data = await installSnap();
+  });
+});
