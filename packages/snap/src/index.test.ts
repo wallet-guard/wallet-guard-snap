456241af2a8c4c99837489e49fba6d86a5a7ee24
mindofmar <martinpeko777@gmail.com>
2023-06-27 17:02:19 -0500

    add first test

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -1,10 +1,37 @@
 import { expect } from '@jest/globals';
 import { installSnap } from '@metamask/snaps-jest';
 
-import { panel, text } from '@metamask/snaps-ui';
+import { heading, panel, text } from '@metamask/snaps-ui';
+import { ChainId } from './types/chains';
+import { UnsupportedChainComponent } from './components';
+
 
 describe('onTransaction', () => {
-  it('returns transaction insights for an ERC-20 transaction', async () => {
-    const data = await installSnap();
+  describe('supported chains uis', async () => {
+    it('should display unsupported chain UI for unknown chain ID', async () => {
+      const snap = await installSnap();
+
+      const response = await snap.sendTransaction({
+        chainId: '0x123',
+      });
+
+      const expected = panel([
+        heading('Unsupported chain'),
+        text(
+          'We will be adding support for more chains very soon. Head to our Discord to suggest which one we support next!',
+        ),
+      ]);
+
+      expect(response).toRender(expected);
+      await snap.close();
+    });
+
+    // snap.sendTransaction({
+    //   chainId: ChainId.EthereumMainnet,
+
+    // })
+
+    // snap.mock
+
   });
 });
