ca75e144b747b0d538c9596bda1957abaf2af781
mar <72634565+mindofmar@users.noreply.github.com>
2023-10-10 16:27:10 -0500

    Feature: 🔴 Optimism simulation support (#30)
    * add optimism chain support
    
    * fix chain id

--- packages/snap/src/types/chains.ts
+++ packages/snap/src/types/chains.ts
@@ -2,4 +2,5 @@ export enum ChainId {
   EthereumMainnet = 'eip155:1',
   PolygonMainnet = 'eip155:89',
   ArbitrumMainnet = 'eip155:a4b1',
+  OptimismMainnet = 'eip155:a',
 }
--- packages/snap/src/utils/environment.ts
+++ packages/snap/src/utils/environment.ts
@@ -1,1 +1,2 @@
+// Security API - For business inquiries please visit https://docs.walletguard.app
 export const SERVER_BASE_URL = 'https://api.walletguard.app/snaps';
