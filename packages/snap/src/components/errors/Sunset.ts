be3b23ce1be8c5ef20d5aac81b8b105c06c0ee35
mindofmar <martin.peko@consensys.net>
2025-04-24 16:04:44 -0500

    update verbaiage

--- packages/snap/src/components/errors/Sunset.ts
+++ packages/snap/src/components/errors/Sunset.ts
@@ -4,8 +4,8 @@ export const SunsetComponent = (): Panel => {
   return panel([
     heading('Sunset notice'),
     text(
-      `The Wallet Guard Snap has been sunset. Please review the article below for details.`,
+      `The Wallet Guard Snap has been sunset and is no longer supported. Features of Wallet Guard are now available directly within MetaMask. Please review the article below for details.`,
     ),
-    copyable('https://www.walletguard.app/blog/wallet-guard-sunset-notice')
+    copyable('https://www.walletguard.app/blog/wallet-guard-sunset-notice'),
   ]);
 };
